1936736a0e1a76333f7142b1de4985577440602c
fdl-11 <fadlidarusalam11@gmail.com>
2023-06-12 05:01:19 -0400

    learn

---
+++ learn_bash/learn2.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# this is a comment
+echo "Hello World" # this is also a comment
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+name=Mark
+VALUE=10
+echo $name
+echo My name is $name
+echo value $VALUE
---
+++ learn_bash/learn3.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# READ USER INPUT
+
+# One input
+echo "Enter name : "
+read name
+echo "Ur name : $name"
+
+echo -e
+
+# Multiple Input
+echo "Enter multiples name : "
+read name1 name2 name3
+echo "Ur name : $name1 $name2 $name3"
+
+echo -e
+
+# Input in same line
+read -p "username : " user
+echo "Ur username : $user"
+
+echo -e
+
+# Silent/Hidden Input
+read -p "username : " username
+read -sp "password : " password
+echo -e
+echo username : $username
+echo password : $password
+
+echo -e
+
+# Multiple Input and Stored in Array
+echo "Enter names : "
+read -a names
+echo "Names : ${names[0]}, ${names[1]}"
+
+# Using built-in variables to storing value
+echo "Enter name : "
+read
+echo $REPLY
